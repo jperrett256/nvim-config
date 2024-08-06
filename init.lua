fb6b695c1c148b48a927573a223615f050902ee6
Joshua Perrett <jperrett256@gmail.com>
2024-08-06 16:36:53 +0100

    improved Neovide config platform compatibility
    The guifont option is now only set for Windows. The default font/size
    on MacOS seems fine, meaning this config works for MacOS now as well.

--- init.lua
+++ init.lua
@@ -4,7 +4,9 @@ require("remap")
 require("netrw")
 
 if vim.g.neovide then
-  vim.o.guifont = "Cascadia Mono:h11"
+  if vim.fn.has("win32") == 1 then
+    vim.o.guifont = "Cascadia Mono:h11"
+  end
   vim.g.neovide_scroll_animation_length = 0.05
   -- vim.g.neovide_cursor_animation_length = 0
   vim.g.neovide_cursor_trail_size = 0.3
